9e55adb51c46f89d58594bc72dc54402fe1adf6b
yasarbudak <yasar.budak@hotmail.com>
2023-11-04 20:16:29 +0300

    Add project files.

---
+++ Continuous-Mean-Value-Calculation.ConsoleAPP/Program.cs
@@ -0,0 +1,25 @@
+﻿int mean = 0;
+int count = 0;
+
+while (true)
+{
+  Console.Write("Enter a value (Press 'x' to exit): ");
+  string inputValue = Console.ReadLine();
+
+  if (inputValue.ToLower() == "x")
+  {
+    break;
+  }
+
+  if (int.TryParse(inputValue, out int value))
+  {
+    mean = (mean * count + value) / count + 1;
+    count++;
+
+        Console.WriteLine("New mean: " + mean);
+    }
+  else
+  {
+    Console.WriteLine("You have to enter a numeric value or press 'x' to exit.");
+  }
+}
